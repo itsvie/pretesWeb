bce9714adb5106f830fa5ce6632649de45e0122d
itsvie <112785102+itsvie@users.noreply.github.com>
2023-08-28 13:25:09 +0700

    Add files via upload

---
+++ PreTest/js/script.js
@@ -0,0 +1,12 @@
+// toggle class active
+const navbarnav = document.querySelector('.navbar-nav')
+document.querySelector('#hamburger-menu'). onclick = () => {
+    navbarnav.classList.toggle('active');
+}
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+        navbarnav.classList.remove('active');
+    }
+})
